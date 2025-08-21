68cb0f0673393fb9919321ab9b47cc279a02ae99
개발자 ktw <rnjsxodhks0719@gmail.com>
2025-08-21 14:35:07 +0900

    fix main

--- aes_main_process.py
+++ aes_main_process.py
@@ -549,7 +549,7 @@ if __name__ == "__main__":
     red_e_time = time.time()
     print(f"noise reduction complete!!! Time taken: {red_e_time - red_s_time} seconds")
     
-    verify = verify_round_output(engine_context, enc_data_hi_round_1, enc_data_lo_round_1, ground_truth= [0x04, 0xe0, 0x48, 0x28, 0x66, 0xcb, 0xf8, 0x06, 0x81, 0x19, 0xd3, 0x26, 0xe4, 0x9a, 0x7a, 0x4c], mode=mode_choice)
+    verify = verify_round_output(engine_context, enc_data_hi_round_1, enc_data_lo_round_1, ground_truth = [0xd4, 0xe0, 0xb8, 0x1e, 0xbf, 0xb4, 0x41, 0x27, 0x5d, 0x52, 0x11, 0x98, 0x30, 0xae, 0xf1, 0xe5], mode=mode_choice)
     
     mix_s_time = time.time()
     enc_data_hi_round_1, enc_data_lo_round_1 = mix_columns(engine_context, enc_data_hi_round_1, enc_data_lo_round_1)
